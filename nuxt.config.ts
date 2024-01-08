fdbe29f3231aad452062589cf6069dd434e83e0d
shuvro111 <iamshuvrosarkar@gmail.com>
2024-01-08 20:54:38 +0530

    ✨ feat: added prismic and slicemachine

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
-  modules: ['@nuxt/ui'],
+
+  modules: ['@nuxt/ui', '@nuxtjs/prismic'],
   css: ['~/assets/css/main.css'],
+
+  prismic: {
+    endpoint: 'shuvro',
+  },
 })
