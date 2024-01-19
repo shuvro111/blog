afe946f45351af76f2035f8f5772b37bf82f0ebd
shuvro111 <iamshuvrosarkar@gmail.com>
2024-01-19 15:48:32 +0530

    ♻️ refactor: Refactor projects page to use dynamic meta tags and slice components

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,18 +1,20 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import { components } from '~/slices'
+
+const prismic = usePrismic()
+const { data: page } = useAsyncData('[projects]', () =>
+  prismic.client.getSingle('projects'))
+
 useSeoMeta({
-  title: 'Projects',
-  description: 'Projects',
+  title: page.value?.data.meta_title,
+  description: page.value?.data.meta_description,
 })
 </script>
 
 <template>
-  <div>
-    <h1>
-      Coming soon...
-    </h1>
-  </div>
+  <SliceZone
+    wrapper="main"
+    :slices="page?.data.slices ?? []"
+    :components="components"
+  />
 </template>
-
-<style>
-
-</style>
